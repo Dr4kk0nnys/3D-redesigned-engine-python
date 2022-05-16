71c5140876310ae06ab1b65661a65cd2a7833228
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2022-05-16 14:33:02 -0300

    Not working demo

--- src/camera.py
+++ src/camera.py
@@ -1,41 +1,80 @@
 import math
 import numpy as np
+import pygame as pg
+# from utils import matrix_functions
 
 
-def view(render, position):
-    position = np.array([*position, 1.0])
-    forward = np.array([0, 0, 1, 1])
-    up = np.array([0, 1, 0, 1])
-    right = np.array([1, 0, 0, 1])
-    h_fov = math.pi / 3
-    v_fov = h_fov * (render.HEIGHT / render.WIDTH)
-    near_plane = 0.1
-    far_plane = 100
+class Camera:
+    def __init__(self, render, position):
+        self.render = render
+        self.position = np.array([*position, 1.0])
+        self.forward = np.array([0, 0, 1, 1])
+        self.up = np.array([0, 1, 0, 1])
+        self.right = np.array([1, 0, 0, 1])
+        self.h_fov = math.pi / 3
+        self.v_fov = self.h_fov * (render.HEIGHT / render.WIDTH)
+        self.near_plane = 0.1
+        self.far_plane = 100
+        # self.moving_speed = 0.3
+        # self.rotation_speed = 0.015
 
+    # def control(self):
+    #     key = pg.key.get_pressed()
+    #     if key[pg.K_a]:
+    #         self.position -= self.right * self.moving_speed
+    #     if key[pg.K_d]:
+    #         self.position += self.right * self.moving_speed
+    #     if key[pg.K_w]:
+    #         self.position += self.forward * self.moving_speed
+    #     if key[pg.K_s]:
+    #         self.position -= self.forward * self.moving_speed
+    #     if key[pg.K_q]:
+    #         self.position += self.up * self.moving_speed
+    #     if key[pg.K_e]:
+    #         self.position -= self.up * self.moving_speed
 
-def translate_matrix(position):
-    x, y, z, w = position
+    #     if key[pg.K_LEFT]:
+    #         self.camera_yaw(-self.rotation_speed)
+    #     if key[pg.K_RIGHT]:
+    #         self.camera_yaw(self.rotation_speed)
+    #     if key[pg.K_UP]:
+    #         self.camera_pitch(-self.rotation_speed)
+    #     if key[pg.K_DOWN]:
+    #         self.camera_pitch(self.rotation_speed)
 
-    return np.array([
-        [1, 0, 0, 0],
-        [0, 1, 0, 1],
-        [0, 0, 1, 0],
-        [-x, -y, -z, 1]
-    ])
+    # def camera_yaw(self, angle):
+    #     rotate = rotate_y(angle)
+    #     self.forward = self.forward @ rotate
+    #     self.right = self.right @ rotate
+    #     self.up = self.up @ rotate
 
+    # def camera_pitch(self, angle):
+    #     rotate = rotate_x(angle)
+    #     self.forward = self.forward @ rotate
+    #     self.right = self.right @ rotate
+    #     self.up = self.up @ rotate
 
-def rotate_matrix(right, forward, up):
-    rx, ry, rz, w = right
-    fx, fy, fz, w = forward
-    ux, uy, uz, w = up
+    def translate_matrix(self):
+        x, y, z, w = self.position
 
-    return np.array([
-        [rx, ux, fx, 0],
-        [ry, uy, fy, 0],
-        [rz, uz, fz, 0],
-        [0, 0, 0, 1]
-    ])
+        return np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 1],
+            [0, 0, 1, 0],
+            [-x, -y, -z, 1]
+        ])
 
+    def rotate_matrix(self):
+        rx, ry, rz, w = self.right
+        fx, fy, fz, w = self.forward
+        ux, uy, uz, w = self.up
 
-def camera_matrix():
-    return translate_matrix() @ rotate_matrix()
+        return np.array([
+            [rx, ux, fx, 0],
+            [ry, uy, fy, 0],
+            [rz, uz, fz, 0],
+            [0, 0, 0, 1]
+        ])
+
+    def camera_matrix(self):
+        return self.translate_matrix() @ self.rotate_matrix()
